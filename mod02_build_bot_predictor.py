e5dd30feb8ff83ac2819150534841e460405e1bd
mikayla <mjr23027@uconn.edu>
2026-09-15 14:09:22 -0400

    try 1

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,7 +10,7 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.1,
+        learning_rate=1,
         n_estimators=100,
         max_depth=8,
         subsample=1,
